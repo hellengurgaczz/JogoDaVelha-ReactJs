9fc3009bcbffe9cf2afa4e862e3e31600d500f36
hellengurgaczz <63474327+hellengurgaczz@users.noreply.github.com>
2021-04-08 23:34:44 -0300

    add

--- src/componentes/jogo.jsx
+++ src/componentes/jogo.jsx
@@ -18,31 +18,29 @@ const jogo = _ => {
     const [solicitarRetorno, setSolicitarRetorno] = React.useState(1)
 
     return (
-        <div id="telaTotalJogo">
-            <div id="jogo">
-                <div id="esquerdo">
+
+        <div id="jogo">
+            <div id="esquerdo">
+                <div id="menu">
                     <h2>Jogo</h2>
                     <h3>Jogador: {jogadorIcone}</h3>
                     {ganhador === 'X' || ganhador === 'O' ? <p className={ganhador}>O jogador {ganhador} vanceu! Parabens!</p> : null}
                     {ganhador === '#' ? <p className={'velha'}>DEU VELHA! &#129325; </p> : null}
                     <button onClick={e => window.location.reload()}>REINICIAR JOGO</button>
                 </div>
-                <div id="direito">
-                    <Tabuleiro icone={jogadorIcone} mudarIcone={mudarIcone} declararGanhador={declararGanhador} ganhador={ganhador} />
-                </div>
-            </div>
-            <div id="desafios">
                 <div id="opcoes">
                     <h3>Lista de Jogadas:</h3>
-                    <button onClick={e => setSolicitarRetorno(1)}>Ordem crescente</button>
+                    <button onClick={e => setSolicitarRetorno(1)}>Ordem crescente</button><br />
                     <button onClick={e => setSolicitarRetorno(2)}>Ordem descrescente</button>
                 </div>
+            </div>
+            <div id="direito">
+                <Tabuleiro icone={jogadorIcone} mudarIcone={mudarIcone} declararGanhador={declararGanhador} ganhador={ganhador} />
                 <div id="retornos">
                     {solicitarRetorno ? <Retorno solicitarRetorno={solicitarRetorno} /> : null}
                 </div>
             </div>
         </div>
-
     )
 }
 
